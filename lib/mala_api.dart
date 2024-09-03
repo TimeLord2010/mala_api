ba6608529f5f06bd6982b9f1ffa504a0cc3439c0
Vinicius <timelord1025@gmail.com>
2024-09-03 10:58:19 -0300

    feat: update server url

--- lib/mala_api.dart
+++ lib/mala_api.dart
@@ -6,4 +6,5 @@ library;
 export 'src/data/index.dart';
 export 'src/factories/logger.dart';
 export 'src/mala_api.dart';
+export 'src/usecases/entities/logs/insert_remote_log.dart';
 export 'src/usecases/object/index.dart';
--- lib/src/data/models/configuration.dart
+++ lib/src/data/models/configuration.dart
@@ -1,6 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:mala_api/src/factories/index.dart';
+
 import '../enums/app_operation_mode.dart';
 
 class Configuration {
   static bool isDebugMode = false;
   static AppOperationMode mode = AppOperationMode.hybrid;
+
+  static void updateUrl(String url) {
+    dio = Dio(BaseOptions(
+      baseUrl: url,
+    ));
+  }
 }
