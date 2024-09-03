7f95a0c6a1e8408d7c098221e6d01c501e91bf5b
Vinicius <timelord1025@gmail.com>
2024-09-03 10:30:57 -0300

    fix: pdf configuration

--- lib/src/modules/tag_pdf_configuration.dart
+++ lib/src/modules/tag_pdf_configuration.dart
@@ -4,46 +4,46 @@ class TagPdfConfiguration {
   // MARK: Dimensions
 
   double get tagWidth => local.getTagWidth();
-  Future<void> setTagWidth(double value) async {
+  Future<void> setTagWidth(double? value) async {
     await local.setTagWidth(value);
   }
 
   double get tagHeight => local.getTagHeight();
-  Future<void> setTagHeight(double value) async {
+  Future<void> setTagHeight(double? value) async {
     await local.setTagHeight(value);
   }
 
   // MARK: Margin
 
   double get tagLeftMargin => local.getTagLeftMargin();
-  Future<void> setTagLeftMargin(double value) async {
+  Future<void> setTagLeftMargin(double? value) async {
     await local.setTagLeftMargin(value);
   }
 
   double get tagRightMargin => local.getTagRightMargin();
-  Future<void> setTagRightMargin(double value) async {
+  Future<void> setTagRightMargin(double? value) async {
     await local.setTagRightMargin(value);
   }
 
   double get tagTopMargin => local.getTagTopMargin();
-  Future<void> setTagTopMargin(double value) async {
+  Future<void> setTagTopMargin(double? value) async {
     await local.setTagTopMargin(value);
   }
 
   double get tagBottomMargin => local.getTagBottomMargin();
-  Future<void> setTagBottomMargin(double value) async {
+  Future<void> setTagBottomMargin(double? value) async {
     await local.setTagBottomMargin(value);
   }
 
   // MARK: Spacings
 
   double get tagHorizontalSpacing => local.getTagHorizontalSpacing();
-  Future<void> setTagHorizontalSpacing(double value) async {
+  Future<void> setTagHorizontalSpacing(double? value) async {
     await local.setTagHorizontalSpacing(value);
   }
 
   double get tagVerticalSpacing => local.getTagVerticalSpacing();
-  Future<void> setTagVerticalSpacing(double value) async {
+  Future<void> setTagVerticalSpacing(double? value) async {
     await local.setTagVerticalSpacing(value);
   }
 }
