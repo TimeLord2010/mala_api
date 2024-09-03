f4f83666f4783bde487c0e1b5dd719885933174b
Vinicius <timelord1025@gmail.com>
2024-09-03 10:11:31 -0300

    feat: user module

--- lib/src/mala_api.dart
+++ lib/src/mala_api.dart
@@ -3,11 +3,13 @@ import 'package:mala_api/src/modules/patient_module.dart';
 import 'package:mala_api/src/modules/pdf_module.dart';
 import 'package:mala_api/src/modules/server_module.dart';
 import 'package:mala_api/src/modules/tag_pdf_configuration.dart';
+import 'package:mala_api/src/modules/user_module.dart';
 
 class MalaApi {
-  static final patient = PatientModule();
-  static final server = ServerModule();
-  static final pdf = PdfModule();
-  static final tagPdfConfig = TagPdfConfiguration();
   static final file = FileModule();
+  static final patient = PatientModule();
+  static final pdf = PdfModule();
+  static final server = ServerModule();
+  static final tagPdfConfig = TagPdfConfiguration();
+  static final user = UserModule();
 }
---
+++ lib/src/modules/user_module.dart
@@ -0,0 +1,11 @@
+import 'package:mala_api/src/usecases/entities/index.dart';
+
+class UserModule {
+  Future<void> login(String email, String password) async {
+    await loginUser(email, password);
+  }
+
+  Future<void> logout() async {
+    await signout();
+  }
+}
