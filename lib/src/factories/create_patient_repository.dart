70181cb365fe06e48788b543afed146eac9f82cc
Vinicius <timelord1025@gmail.com>
2024-09-03 12:03:17 -0300

    fix: patient repository factory

--- lib/src/factories/create_patient_repository.dart
+++ lib/src/factories/create_patient_repository.dart
@@ -1,14 +1,22 @@
 import 'package:mala_api/src/data/interfaces/patient_interface.dart';
+import 'package:mala_api/src/repositories/index.dart';
 
 import '../data/enums/app_operation_mode.dart';
 import '../data/models/configuration.dart';
-import '../repositories/patient_repository/local_patient_repository.dart';
 import 'create_database_client.dart';
 
 Future<PatientInterface> createPatientRepository() async {
   return switch (Configuration.mode) {
-    AppOperationMode.offline =>
-      LocalPatientRepository(await createDatabaseClient()),
+    AppOperationMode.offline => await _createLocal(),
+    AppOperationMode.hybrid => HybridPatientRepository(
+        localRepository: await _createLocal(),
+        onlineRepository: OnlinePatientRepository(),
+      ),
     _ => throw Exception('Not implemented'),
   };
+}
+
+Future<LocalPatientRepository> _createLocal() async {
+  var isar = await createDatabaseClient();
+  return LocalPatientRepository(isar);
 }
