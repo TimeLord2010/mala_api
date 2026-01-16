65d71cff4a1874a615df30bee04658bd1b70d24f
Vinicius G <timelord1025@gmail.com>
2026-01-16 11:04:08 -0300

    feat: allow untrusted certificates

--- lib/src/data/models/configuration.dart
+++ lib/src/data/models/configuration.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
 import 'package:mala_api/src/factories/index.dart';
 
 import '../enums/app_operation_mode.dart';
@@ -8,8 +10,15 @@ class Configuration {
   static AppOperationMode mode = AppOperationMode.hybrid;
 
   static void updateUrl(String url) {
-    dio = Dio(BaseOptions(
-      baseUrl: url,
-    ));
+    final newDio = Dio(BaseOptions(baseUrl: url));
+
+    // Configure HttpClientAdapter to accept all certificates
+    (newDio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = () {
+      final client = HttpClient();
+      client.badCertificateCallback = (cert, host, port) => true;
+      return client;
+    };
+
+    dio = newDio;
   }
 }
--- lib/src/factories/http_client.dart
+++ lib/src/factories/http_client.dart
@@ -1,5 +1,22 @@
+import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
 
-var dio = Dio(BaseOptions(
-  baseUrl: 'https://aj18h1vzgh.execute-api.us-east-1.amazonaws.com/Prod/',
-));
+// Helper function to create Dio client with certificate bypass
+Dio _createDioClient(String baseUrl) {
+  final dio = Dio(BaseOptions(baseUrl: baseUrl));
+
+  // Configure HttpClientAdapter to accept all certificates
+  (dio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = () {
+    final client = HttpClient();
+    client.badCertificateCallback = (cert, host, port) => true;
+    return client;
+  };
+
+  return dio;
+}
+
+// Initialize with certificate bypass
+var dio = _createDioClient(
+  'https://aj18h1vzgh.execute-api.us-east-1.amazonaws.com/Prod/',
+);
