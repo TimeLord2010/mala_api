18cce0812c8449f9d1b4937875339871026faa3b
Vinícius Gabriel <timelord1025@gmail.com>
2026-01-26 23:18:57 -0300

    .

--- lib/src/usecases/entities/logs/insert_remote_log.dart
+++ lib/src/usecases/entities/logs/insert_remote_log.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:system_info2/system_info2.dart';
 import 'package:vit_dart_extensions/vit_dart_extensions.dart';
 
 import '../../../factories/http_client.dart';
@@ -17,18 +16,6 @@ Future<void> insertRemoteLog({
   var logger = createSdkLogger('insertRemoteLog');
 
   try {
-    int getVirtualMemorySize() {
-      try {
-        return SysInfo.getVirtualMemorySize();
-      } catch (_) {
-        return -1;
-      }
-    }
-
-    var freeVMem = SysInfo.getFreeVirtualMemory();
-    var totalVMem = SysInfo.getTotalVirtualMemory();
-    var freePMemory = SysInfo.getFreePhysicalMemory();
-    var totalPMemory = SysInfo.getTotalPhysicalMemory();
 
     var virtualMemorySize = getVirtualMemorySize();
     await dio.post(
@@ -38,15 +25,9 @@ Future<void> insertRemoteLog({
         'message': message,
         'level': level,
         if (stack != null) ...{'stack': stack},
-        'pcName': SysInfo.userName,
         'extras': {
           'cpus': Platform.numberOfProcessors,
           'platform': Platform.operatingSystem,
-          'ram': {
-            'virtual': _usage(freeVMem, totalVMem),
-            'physical': _usage(freePMemory, totalPMemory),
-            'v_used': virtualMemorySize,
-          },
           if (extras != null) ...extras,
         },
       },
@@ -55,12 +36,4 @@ Future<void> insertRemoteLog({
     var msg = getErrorMessage(e) ?? 'Falha ao enviar logs';
     logger.e('(insertRemoteLog) $msg');
   }
-}
-
-Map<String, dynamic> _usage(int free, int total) {
-  return {
-    'free': free.readableByteSize(),
-    'total': total.readableByteSize(),
-    '% free': ((free / total) * 100).toStringAsFixed(2),
-  };
 }
